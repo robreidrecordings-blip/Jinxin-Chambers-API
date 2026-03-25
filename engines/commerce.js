6baf0917cbbba052f1ae60393b4ff5751cb27347
robreidrecordings-blip <robreidrecordings@gmail.com>
2026-03-25 04:14:22 +0000

    Add support for new store configuration and routes

--- engines/commerce.js
+++ engines/commerce.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 // ── Store configurations ───────────────────────────────────────────────────────
 const stores = {
-  
+  com: {
+    url:            process.env.WC_COM_URL            || 'https://thecopperglowshop.com',
+    consumerKey:    process.env.WC_COM_CONSUMER_KEY,ck_abd69adccb7195fb52462acca03479d3e9fbcb17
+   consumerSecret: process.env.cs_0757cba26afe2fb757091c045caedaae8f25df11
+  },
   co_uk: {
     url:            process.env.WC_CO_UK_URL            || 'https://thecopperglowshop.co.uk',
     consumerKey:    process.env.WC_CO_UK_CONSUMER_KEY,ck_4407d6335b0fb5a268c22a01104a93c491d4f1e6
